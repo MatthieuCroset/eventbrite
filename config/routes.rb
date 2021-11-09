6fee6e25e18907510cc2de5cca079db61e0bfd24
Matthieu Croset <crosetmatthieu@gmail.com>
2021-11-09 11:29:31 +0100

    Event page  to Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
   root 'events#index'
-  get 'events/index'
-  get 'events/show'
-  get 'events/new'
-  get 'events/create'
-  get 'events/edit'
-  get 'events/update'
-  get 'events/destroy'
+  resources :events
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
