cf4c17c8b6fce269c0f4809fd2f399d516eac4a3
Matthieu Croset <crosetmatthieu@gmail.com>
2021-11-09 11:20:22 +0100

    add root  to Heroku

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,25 @@
+class EventsController < ApplicationController
+  def index
+    @events = []
+    Event.order(:id).reverse.each do |event|
+      @events << [event, User.find(event.user_id)]
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  root 'events#index'
+  get 'events/index'
+  get 'events/show'
+  get 'events/new'
+  get 'events/create'
+  get 'events/edit'
+  get 'events/update'
+  get 'events/destroy'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
